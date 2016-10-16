2f11d4d55c969343b5b09619c465245e014d436d
Leonardo Soares <leonardogcsoares93@gmail.com>
2016-10-16 18:41:22 -0200

    Added RWLock for threadsafety

--- set.go
+++ set.go
@@ -1,35 +1,52 @@
 package goset
 
+import "sync"
+
 type Set struct {
+	sync.RWMutex
 	set map[interface{}]bool
 }
 
 func NewSet() *Set {
-	return &Set{make(map[interface{}]bool)}
+	return &Set{set: make(map[interface{}]bool)}
 }
 
 func (set *Set) Add(i interface{}) bool {
+	set.Lock()
 	_, exists := set.set[i]
 	set.set[i] = true
+	set.Unlock()
 	return !exists
 }
 
 func (set *Set) Size() int {
+	set.RLock()
+	defer set.RUnlock()
+
 	return len(set.set)
 }
 
 func (set *Set) isMember(i interface{}) bool {
+	set.RLock()
 	_, exists := set.set[i]
+	set.RUnlock()
 	return exists
 }
 
 func (set *Set) del(i interface{}) {
+	set.Lock()
 	delete(set.set, i)
+	set.Unlock()
 }
 
 func (A *Set) Union(B *Set) *Set {
+	A.RLock()
+	defer A.RUnlock()
+	B.RLock()
+	defer B.RUnlock()
+
 	newSet := NewSet()
-	for key, _ := range A.set {
+	for key := range A.set {
 		newSet.Add(key)
 	}
 
@@ -51,11 +68,17 @@ func (A *Set) merge(B *Set) *Set {
 }
 
 func (A *Set) Intersect(B *Set) *Set {
+	A.RLock()
 	ACard := A.Size()
+	A.RUnlock()
+
+	B.RLock()
 	BCard := B.Size()
+	B.RUnlock()
+
 	if ACard > BCard {
 		return A.merge(B)
-	} else {
-		return B.merge(A)
 	}
+
+	return B.merge(A)
 }
